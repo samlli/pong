556fb0678f9cbcb955ca84ddf96aa2cc356ae81d
samlli <samuelli.pk@gmail.com>
2020-11-07 20:05:11 -0500

    pong wrapper module

--- ball.v
+++ ball.v
@@ -1,22 +1,59 @@
-// handles ball movement
-module ball(inX, inY, velX, velY, width, clk, reset, outX, outY);
-    input[9:0] inX; // X Y coords of top left corner
-    input[8:0] inY;
-    input[5:0] velX, velY;
+// outputs X and Y coordinates of upper left corner of ball
+module ball(width, clk, reset, outX, outY);
     input[5:0] width; // width can range from 0-63
     input clk, reset;
 
     output reg[9:0] outX;
     output reg[8:0] outY;
+    // output reg[5:0] outDx, outDy;
+
+    reg bounceX, bounceY;
+
+    reg[5:0] dx, dy;
+    wire[5:0] dxval, dyval;
+
+    assign dxval = dx;
+    assign dyval = dy;
+    // assign dx = (outX<10 || outX+width>630) ? 6'b111111 : 6'b000001;
+    // assign dy = (outY<10 || outY+width>470) ? 6'b111111 : 6'b000001;
 
     always @(posedge clk or posedge reset) begin
         if(reset) begin // ball starts in middle
             outX <= 310-(width>>1);
             outY <= 240-(width>>1);
+
+            dx <= 6'b1;
+            dy <= 6'b1;
+            bounceX <= 0;
+            bounceY <= 0;
         end
         else begin
-            outX <= inX + velX;
-            outY <= inY + velY;
+            outX <= outX + dx;
+            outY <= outY + dy;
+
+            // dx <= (outX<10 || outX+width>630) ? -dxval : dxval;
+            // dy <= (outY<10 || outY+width>470) ? -dyval : dyval;
+
+            bounceX <= (outX<10 || outX+width>630) ? 1 : 0;
+            bounceY <= (outY<10 || outY+width>470) ? 1 : 0;
+
+            // if(outX<10 || outX+width>630) ? 1 : 0;
+            // bounceY <=
+            //     bounceX <= 1;
+            // else
+            //     bounceX <= 0;
+            // if(outY<10 || outY+width>470)
+            //     bounceY <= 1;
+            // else
+            //     bounceY <= 0;
         end
+    end
+    always @(posedge bounceX) begin
+        dx <= -dxval;
+        // dx <= (outX<10 || outX+width>630) ? -dxval : dxval;
+    end
+    always @(posedge bounceY) begin
+        dy <= -dyval;
+        // dy <= (outY<10 || outY+width>470) ? -dyval : dyval;
     end
 endmodule
--- pong.v
+++ pong.v
@@ -1,5 +1,43 @@
-module pong():
+module pong(
+    input clk, 			// 100 MHz System Clock
+    input reset, 		// Reset Signal
+    output hSync, 		// H Sync Signal
+    output vSync, 		// Veritcal Sync Signal
+    output[3:0] VGA_R,  // Red Signal Bits
+    output[3:0] VGA_G,  // Green Signal Bits
+    output[3:0] VGA_B,  // Blue Signal Bits
+    inout ps2_clk,
+    inout ps2_data);
 
+    // Clock divider 100 MHz -> 100 Hz
+	wire clk100; // 25MHz clock
+
+	reg[19:0] pixCounter = 0;      // Pixel counter to divide the clock
+	assign clk100 = pixCounter[19]; // Set the clock high whenever the second bit (2) is high
+	always @(posedge clk) begin
+		pixCounter <= pixCounter + 1; // Since the reg is only _ bits, it will reset every _ cycles
+	end
+
+    // calculate ball position
+    // draw ball, upper left coords
+	wire[9:0] ball_x;
+	wire[8:0] ball_y;
+	reg[5:0] ball_width = 6'b100000; // width of ball
+
+    ball ball_loc(.width(ball_width), .clk(clk100), .reset(reset), .outX(ball_x), .outY(ball_y));
+
+    // calculate paddle position
+    // paddle_left
+
+    // paddle_right
+
+
+    // VGAController for drawing on screen
+    VGAController board(.clk(clk), .reset(reset), .hSync(hSync), .vSync(vSync), .VGA_R(VGA_R), .VGA_G(VGA_G), .VGA_B(VGA_B), .ps2_clk(ps2_clk), .ps2_data(ps2_data), .ball_x(ball_x), .ball_y(ball_y), .ball_width(ball_width));
+
+
+
+    // FSM counter for scoring
 
 
 endmodule
